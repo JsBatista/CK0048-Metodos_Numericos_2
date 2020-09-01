28d891a286c5b5f41f2f0de6e7ce1004104c5b1f
JsBatista <joaobatistafreitasfilho@gmail.com>
2020-09-01 14:54:50 -0300

    Filtering results

--- src/OnePointBoundary.cpp
+++ src/OnePointBoundary.cpp
@@ -16,36 +16,69 @@ void OnePointBoundary::eulerExplicitProblem1(int iterations, double v0, double y
     double g = 10;
 
     double v1, y1;
-    double max = -999;
-    double maxT, yT, maxI;
-    std::cout << "T = " << t0 << std::endl;
-    std::cout << "V0: " << v0 << std::endl;
-    std::cout << "Y0: " << y0 << std::endl << std::endl;
+    double maxY = y1;
+    double maxT = t0;
+    double maxI = 0;
+
+    bool onOcean = false;
         
     for(int i = 0; i < iterations; i++)
     {
+
         v1 = v0 + deltaT*(-g-(k*v0/m));
         y1 = y0 + deltaT*v0;
-        std::cout << "T = " << t0 + i*deltaT + deltaT << std::endl;
-        std::cout << "V" << i+1 << " : " << v1 << std::endl;
-        std::cout << "Y" << i+1 << " : " << y1 << std::endl << std::endl;
+        
         v0 = v1;
         y0 = y1;
-        /*if(max < y1)
+
+        // Registro da altura máxima do objeto
+        if(maxY < y1)
         {
-            maxT = i*deltaT + deltaT;
-            yT = y1;
-            maxI = i;
-        }*/
-        if(y1 <= -0.1)
+            maxY = y1;
+            maxT = t0 + i*deltaT + deltaT;
+            maxI = i+1;
+        }
+
+        // Verificação se o objeto chegou ao nível no mar, mostrando na tela quando isso ocorreu e com qual velocidade
+        // Como raramente o objeto chega em exatamente 0, são mostrados os 2 passos, imediatamente antes e após a chegada do objeto no nível do mar
+        if( y0 + deltaT*v0 < 0)
         {
-            i = iterations;
+
+            std::cout << "Passos relevantes quanto à queda no mar: " << std::endl;
+            std::cout << "T = " << t0 + i*deltaT + deltaT << std::endl;
+            std::cout << "V" << i+1 << " : " << v1 << std::endl;
+            std::cout << "Y" << i+1 << " : " << y1 << std::endl << std::endl;
+            
+            v1 = v0 + deltaT*(-g-(k*v0/m));
+            y1 = y0 + deltaT*v0;
+            
+            v0 = v1;
+            y0 = y1;
+            i += 1;
+
+            std::cout << "T = " << t0 + i*deltaT + deltaT << std::endl;
+            std::cout << "V" << i+1 << " : " << v1 << std::endl;
+            std::cout << "Y" << i+1 << " : " << y1 << std::endl << std::endl;
+
+            onOcean = true;
+            i = iterations; 
         }
     }
 
-    //std::cout << "T = " << maxT << std::endl;
-    //std::cout << "I  = " << maxI << std::endl;
-    //std::cout << "Yt = " << yT << std::endl;
+    // Mostrando a altura máxima da trajetória e os dados referentes a esse passo
+    std::cout << "Passo da altura máxima na trajetória: " << std::endl;
+    std::cout << "T = " << maxT << std::endl;
+    std::cout << "Y" << maxI << " : " << maxY << std::endl << std::endl;
 
+    // Caso o objeto nunca chegue a cair no nivel do mar
+    if(!onOcean)
+    {
+        std::cout << "Com esse número de passo não foi possível saber quando o objeto cairá no mar. " << std::endl;
+        std::cout << "Dados do último passo: " << std::endl;
+        std::cout << "T = " << t0 + iterations*deltaT << std::endl;
+        std::cout << "V" << iterations << " : " << v1 << std::endl;
+        std::cout << "Y" << iterations << " : " << y1 << std::endl << std::endl;
+    }
+    
     return;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,12 @@ int main()
     // Com deltaT = 0.01 => Altura Máxima: 201.225m em T = 0.49 Passo 49
     //                     Queda no mar: Entre 7.79 e 7.8 Entre passos 779 e 780 com Velocidade entre -47.9181 e -47.9582
     
-    // Com deltaT = 0.001 => Altura Máxima: 201.203 em T = 0.48 Passo 480
+    // Com deltaT = 0.001 => Altura Máxima: 201.203 em T = 0.485 Passo 485
     //                     Queda no mar: Entre T = 7.79 e T = 7.791 Entre passos 7790 e 7791 com Velocidade entre -47.9005 e -47.9045     
    
-    // Com deltaT = 0.0001 => Altura Máxima: 201.2 em T = 0.4799 Passo 4799
+    // Com deltaT = 0.0001 => Altura Máxima: 201.2 em T = 0.485 Passo 4850
     //                     Queda no mar: Entre T = 7.7897  e T = 7.7898  Entre passos 77897 e 77898 com Velocidade entre -47.8975 e -47.8979 
-    opb.eulerExplicitProblem1(80000, 5, 200, 0.25, 2, 0.0001, 0);
+    opb.eulerExplicitProblem1(80000, 5, 200, 0.25, 2, 0.1, 0);
 
     return 0;
 
