0fcb193dda3d2f5e0051b2cef452ec176288b294
JsBatista <joaobatistafreitasfilho@gmail.com>
2020-10-06 15:32:17 -0300

    Automatizing Previous Code

--- src/main.cpp
+++ src/main.cpp
@@ -30,83 +30,101 @@ int main()
     // Preciso questionar minha sanidade calculando um sistema de equações com 49 icógnitas
     
 
+    // Automatização: Definindo o número de partições
+    int N = 8;
+
+    // Automatização: Definindo deltaX e deltaY
+    double deltaX = 1.0/N;
+    double deltaY = 1.0/N;
+
+    // Automatização: Calculando o número de icógnitas no problema bidimensional:
+    int variables = (N-1)*(N-1);
+
+    // Automatização: Nos casos que F(x,y) = C, definir esse C
+    double C = 4;
+
+
     // Calculando matriz 49x4 IN
     std::vector<std::vector<double>> IN = {};
-    for(int i = 0; i < 49; i++){
+    for(int i = 0; i < variables; i++){
         IN.push_back({-1, -1, -1, -1});
     }    
 
     // Percorrendo matriz IN ajustando cada valor para condizer com a máscara:
-    for(int i = 0; i < 49; i++){
+    
+    for(int i = 0; i < variables; i++){
         // Calculando Esquerda
-        // Valores múltiplos de 7 estão na borda
-        if(i % 7 != 0){
+        // Valores múltiplos de N estão na borda
+        if( i % (N-1) != 0){
             IN[i][0] = i-1;
         }
         // Calculando Direita
-        // Valores + 1 múltiplos de 7 estão na borda
-        if( (i+1) % 7 != 0 ){
+        // Valores + 1 múltiplos de N estão na borda
+        if( (i+1) % (N-1) != 0 ){
             IN[i][1] = i+1;
         }
         // Calculando Baixo
         // A primeira fila está na borda
-        if( i >= 7 ){
-            IN[i][2] = i-7;
+        if( i >= (N-1) ){
+            IN[i][2] = i-(N-1);
         }
         // Calculando Cima 
         // A ultima fila está na borda
-        if( i <= 41 ){
-            IN[i][3] = i+7;
+        if( i < variables-(N-1) ){
+            IN[i][3] = i+(N-1);
         }
     } 
     // Caso precise inspecionar IN
-    //Eigenvectors eigen;
-    //eigen.printMatrix(IN);
+    Eigenvectors eigen;
+    eigen.printMatrix(IN);
 
     // Matriz dos coeficientes da máscara
-    std::vector<double> Mask = {64, 64, 64, 64};
-    double A = -256;
+    std::vector<double> Mask = {1.0/(deltaX*deltaX), 1.0/(deltaX*deltaX), 1.0/(deltaY*deltaY), 1.0/(deltaY*deltaY)};
+    double A = -2*(1.0/(deltaX*deltaX) + 1.0/(deltaY*deltaY));
+
+    eigen.printVector(Mask);
+    std::cout << "A: " << A;
 
 
     std::vector<std::vector<double>> SystemMatrix = {};
-    for(int i = 0; i < 49; i++){
+    for(int i = 0; i < variables; i++){
         SystemMatrix.push_back({});
-        for(int j = 0; j < 49; j++){
+        for(int j = 0; j < variables; j++){
             SystemMatrix[i].push_back(0);
         }
     }
+    
 
     // Percorrer IN
-    for(int i = 0; i < 49; i++){
+    for(int i = 0; i < variables; i++){
         // Setar SystemMatrix[i][i] = A
         SystemMatrix[i][i] = A;
-
+        // Automatização: NÃO MEXA NESSE 4, NÃO É IGUAL AO C
         for(int j = 0; j < 4; j ++){
             if(IN[i][j] != -1){
                 SystemMatrix[i][IN[i][j]] = Mask[j];
             }
         }
     }
+    
+    eigen.printMatrix(SystemMatrix);
 
     // Criar vetor b:
     std::vector<double> b = {};
-    for(int i = 0; i < 49; i++){
-        b.push_back(4);
+    for(int i = 0; i < variables; i++){
+        // Automatização: Caso necessário, trocar C por execução de uma função F(x,y)
+        b.push_back(C);
     }
 
     // Usando método de LU
     LU lu;
     std::vector<double> result = lu.LU_partial_pivoting(SystemMatrix, b);
     std::cout << "[ " << std::endl;
-    for(int i = 0; i < 49; i++){
+    for(int i = 0; i < variables; i++){
         std::cout << result[i] << " ," << std::endl;
     }
     std::cout << "] " << std::endl;
-    
-    std::cout << std::endl<< std::endl<< result[8] << std::endl;
-    std::cout << std::endl<< std::endl<< result[12] << std::endl;
-    std::cout << std::endl<< std::endl<< result[36] << std::endl;
-    std::cout << std::endl<< std::endl<< result[40] << std::endl;
+
     return 0;
 
 }
